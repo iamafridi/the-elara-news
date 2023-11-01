bae1d523583659cf0689adf5446e7d4b883d25d7
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-11-01 22:22:02 +0600

    added the lfet catagory

--- src/Pages/Shared/LeftsideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftsideNav/LeftSideNav.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([])
+
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
-        <div>
-            <h2 className="text-2xl">LeftSide Nav</h2>
+        <div className="space-y-4">
+            <h2 className="text-2xl">All Categories :{categories.length}</h2>
+        
+        {
+            categories.map(category => <Link 
+                className="block font-semibold text-xl"
+            key={category.id} 
+            to={`/category/${category.id}`}
+            >{category.name}</Link>)
+        }
+        
         </div>
     );
 };
