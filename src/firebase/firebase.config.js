92dfb79adcd30b9c08fe39b8eab40a3b92a5f251
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-11-01 17:17:38 +0600

    Project Setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0uf-rGgWosXX4vQr6or-Ys9s0KxwUYF8",
+  authDomain: "the-elara-news.firebaseapp.com",
+  projectId: "the-elara-news",
+  storageBucket: "the-elara-news.appspot.com",
+  messagingSenderId: "361377242307",
+  appId: "1:361377242307:web:74c17241b2b5fbd18e9676"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
